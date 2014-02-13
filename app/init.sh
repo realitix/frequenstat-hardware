e5f2cc1b8457c9dd114b1447c5fc95589762aa73
www-data <www-data@raspberrypi>
2014-02-13 20:03:03 +0100

    Optimize init

--- app/init.sh
+++ app/init.sh
@@ -3,11 +3,7 @@
 # On charge les variables
 source ./tracker.conf
 
-service network-manager stop
-service avahi-daemon stop
 ifconfig wlan0 down
-
 iwconfig wlan0 essid any
 iwconfig wlan0 mode monitor
-
 ifconfig wlan0 up
